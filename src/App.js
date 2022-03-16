d0f510e45a6718977edd1b78ae743d103cd15bb9
Chaitanya Varanasi <3372940+chaitanyavaranasi@users.noreply.github.com>
2022-03-16 00:03:55 -0400

    Initial Summit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddItem from "./components/add-item.component";
+import Item from "./components/item.component";
+import ItemsList from "./components/items-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/items"} className="navbar-brand">
+            bezKoder
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/items"} className="nav-link">
+                Items
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/items"]} component={ItemsList} />
+            <Route exact path="/add" component={AddItem} />
+            <Route path="/items/:id" component={Item} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/item.component.js
@@ -0,0 +1,195 @@
+import React, { Component } from "react";
+import ItemDataService from "../services/item.service";
+
+export default class Item extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.getItem = this.getItem.bind(this);
+    this.updatePublished = this.updatePublished.bind(this);
+    this.updateItem = this.updateItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+
+    this.state = {
+      currentItem: {
+        id: null,
+        title: "",
+        description: "",
+        published: false
+      },
+      message: ""
+    };
+  }
+
+  componentDidMount() {
+    this.getItem(this.props.match.params.id);
+  }
+
+  onChangeTitle(e) {
+    const title = e.target.value;
+
+    this.setState(function(prevState) {
+      return {
+        currentItem: {
+          ...prevState.currentItem,
+          title: title
+        }
+      };
+    });
+  }
+
+  onChangeDescription(e) {
+    const description = e.target.value;
+
+    this.setState(prevState => ({
+      currentItem: {
+        ...prevState.currentItem,
+        description: description
+      }
+    }));
+  }
+
+  getItem(id) {
+    ItemDataService.get(id)
+      .then(response => {
+        this.setState({
+          currentItem: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  updatePublished(status) {
+    var data = {
+      id: this.state.currentItem.id,
+      title: this.state.currentItem.title,
+      description: this.state.currentItem.description,
+      published: status
+    };
+
+    ItemDataService.update(this.state.currentItem.id, data)
+      .then(response => {
+        this.setState(prevState => ({
+          currentItem: {
+            ...prevState.currentItem,
+            published: status
+          }
+        }));
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  updateItem() {
+    ItemDataService.update(
+      this.state.currentItem.id,
+      this.state.currentItem
+    )
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          message: "The item was updated successfully!"
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  deleteItem() {
+    ItemDataService.delete(this.state.currentItem.id)
+      .then(response => {
+        console.log(response.data);
+        this.props.history.push('/items')
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { currentItem } = this.state;
+
+    return (
+      <div>
+        {currentItem ? (
+          <div className="edit-form">
+            <h4>Item</h4>
+            <form>
+              <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  value={currentItem.title}
+                  onChange={this.onChangeTitle}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="description"
+                  value={currentItem.description}
+                  onChange={this.onChangeDescription}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>
+                  <strong>Status:</strong>
+                </label>
+                {currentItem.published ? "Published" : "Pending"}
+              </div>
+            </form>
+
+            {currentItem.published ? (
+              <button
+                className="badge badge-primary mr-2"
+                onClick={() => this.updatePublished(false)}
+              >
+                UnPublish
+              </button>
+            ) : (
+              <button
+                className="badge badge-primary mr-2"
+                onClick={() => this.updatePublished(true)}
+              >
+                Publish
+              </button>
+            )}
+
+            <button
+              className="badge badge-danger mr-2"
+              onClick={this.deleteItem}
+            >
+              Delete
+            </button>
+
+            <button
+              type="submit"
+              className="badge badge-success"
+              onClick={this.updateItem}
+            >
+              Update
+            </button>
+            <p>{this.state.message}</p>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Item...</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
